f93f31719e454dd07922df798adffcc6bbca6ede
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2022-09-21 11:56:42 +0400

    Numpy

---
+++ nnumpy.py
@@ -0,0 +1,27 @@
+import numpy as np
+import time
+
+# start_time = time.time()
+# A = np.arange(100000)
+
+# A ** 2
+# Time_1 = time.time() - start_time
+
+# start_time2 = time.time()
+# L = range(10000)
+# [i ** 2 for i in L]
+# Time_2 = time.time() - start_time2
+
+# print(Time_2 / Time_1)
+
+
+
+x = np.array([4,5,6])
+
+print(x)
+print(type(x))
+print(x.shape)
+print(x.size)
+print(x.ndim)
+print(x.nbytes)
+print(x.dtype)
